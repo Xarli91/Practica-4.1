9b55d7155cb49be36c761c0d0ec88c6835ec87ae
Carlos García Murillo <c.g.murillo91@gmail.com>
2023-02-05 14:55:47 +0100

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_4._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Font = new Font(label1.Font, label1.Font.Style ^ FontStyle.Bold);
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Font = new Font(label1.Font, label1.Font.Style ^ FontStyle.Italic);
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Font = new Font(label1.Font, label1.Font.Style ^ FontStyle.Underline);
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
